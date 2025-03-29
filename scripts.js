7685d96ea50951c0c48d64914da6eb84a0bec4fd
eric-bittu <eric.silva2@edu.fecap.br>
2025-03-29 00:39:50 -0300

    Feito o sobre
    Criado o sobre, inserido o código da página inicial feita pelo Yanko.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Seleciona o botão
+const botao = document.querySelector('.container button');
+
+// Adiciona o evento de clique
+botao.addEventListener('click', () => {
+    // Redireciona para a página fase-inicial.html
+    window.location.href = 'sobre.html';
+});
